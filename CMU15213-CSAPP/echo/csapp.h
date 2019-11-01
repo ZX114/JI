74c965d1b44f430ebc4ad178e19137a6a6b207ff
Xu Zhang <xu-zhang@sjtu.edu.cn>
2019-11-01 20:14:42 +0800

    An iterative echo server

---
+++ CMU15213-CSAPP/echo/csapp.h
@@ -0,0 +1,59 @@
+#ifndef CSAPP_H
+#define CSAPP_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+
+typedef struct sockadrr SA;
+
+/* Persistent state for the robust I/O (Rio) package */
+/* $begin rio_t */
+#define RIO_BUFSIZE 8192
+typedef struct {
+    int rio_fd;                /* Descriptor for this internal buf */
+    int rio_cnt;               /* Unread bytes in internal buf */
+    char *rio_bufptr;          /* Next unread byte in internal buf */
+    char rio_buf[RIO_BUFSIZE]; /* Internal buffer */
+} rio_t;
+/* $end rio_t */
+
+/* Misc constants */
+#define	MAXLINE	 8192  /* Max text line length */
+#define MAXBUF   8192  /* Max I/O buffer size */
+#define LISTENQ 1024
+
+void unix_error(char* msg);
+
+/* Process control wrappers */
+pid_t Fork(void);
+
+unsigned int snooze(unsigned int);
+
+typedef void handler_t(int);
+handler_t* Signal(int, handler_t*);
+
+/* sio (signal-safe IO) */
+ssize_t sio_puts(char s[]);
+void sio_error(char s[]);
+
+/* Rio (Robust I/O) package */
+ssize_t rio_readn(int fd, void *usrbuf, size_t n);
+ssize_t rio_writen(int fd, void *usrbuf, size_t n);
+void rio_readinitb(rio_t *rp, int fd); 
+ssize_t	rio_readnb(rio_t *rp, void *usrbuf, size_t n);
+ssize_t	rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen);
+
+int open_clientfd(char *hostname, char *port);
+int open_listenfd(char *port);
+
+#endif  // CSAPP_H
+
